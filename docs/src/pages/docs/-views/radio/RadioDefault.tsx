e9cff953ffb5c701be1b8e7bde9c32a4872238d8
Vu Nguyen <anhvu.nd.0199@gmail.com>
2026-06-01 15:36:07 +0700

    chore(release): core@0.4.16

--- docs/src/pages/docs/-views/radio/RadioDefault.tsx
+++ docs/src/pages/docs/-views/radio/RadioDefault.tsx
@@ -7,7 +7,11 @@ export default function RadioDefault() {
   const [plan, setPlan] = useState('pro')
 
   return (
-    <RadioGroup className='m-auto rounded-2xl border border-slate-200 bg-white p-5 shadow-sm' onValueChange={setPlan} value={plan}>
+    <RadioGroup
+      className='m-auto rounded-2xl border border-slate-200 bg-white p-5 shadow-sm'
+      onValueChange={setPlan}
+      value={plan}
+    >
       <Radio value='free'>Free</Radio>
       <Radio value='pro'>Pro</Radio>
       <Radio value='enterprise'>Enterprise</Radio>
